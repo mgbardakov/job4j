f1cf00540d5fb773f2ce6131e38b2d117f171454
mgbardakov <mgbardakov@gmail.com>
2020-01-12 21:23:02 +0300

    6.4.1. Слово заканчивается с ...[#220869]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+/**
+ * ArrayChar класс определения вадидности постфикса
+ * @author Maxim Bardakov (mgbardakov@gmail.com)
+ * @since 12.01.2020
+ * @version 1.0
+ */
+public class EndsWith {
+
+    /**
+     * Проверка валидности постфикса относительно строки
+     * @param word - массив char из строки
+     * @param post - массив char из постфикса
+     * @return result - валиден/не валиден постфикс
+     */
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = false;
+        for(int i = 0; i < post.length; i++) {
+            if (post[post.length - i - 1] == word[word.length - i - 1]) result = true;
+            else {
+                result = false;
+                break;
+            }
+        }
+        // проверить. что массив word имеет последние элементы одинаковые с post
+        return result;
+    }
+}
