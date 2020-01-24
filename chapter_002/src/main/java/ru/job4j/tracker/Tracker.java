73ade8d6ebc66d8bc0424b7f0a79b527ac241fc6
mgbardakov <mgbardakov@gmail.com>
2020-01-24 23:19:24 +0300

    5. Реализовать класс Tracker[#221039]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,85 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
+
 public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод добавления заявки в хранилище
+     *
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        items[this.position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     *
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        //Реализовать метод генерации.
+        return String.valueOf(new Date().getTime() + new Random().nextLong());
+    }
+
+    /**
+     * Метод возвращает массив всех объектов
+     * @return - массив объектов
+     */
+    public Item[] findAll() {
+        if (position == 0) {
+            return null;
+        }
+        return Arrays.copyOf(items, position);
+    }
+
+    /**
+     * Метод возвращает массив всех объектов с переданным именем
+     * @param key - имя
+     * @return - массив объектов
+     */
+    public Item[] findByName(String key) {
+        Item[] result = new Item[position];
+        int size = 0;
+        for (int index = 0; index < position; index++) {
+            if (items[index].getName().equals(key)) {
+                result[size++] = items[index];
+            }
+        }
+        if (size == 0) {
+            return null;
+        }
+        return Arrays.copyOf(result, size);
+    }
+
+
+    /**
+     * Метод возвращает объект с переданным ID
+     * @param id - ID
+     * @return - найденный объект или null, если объект не найден
+     */
+    public Item findById(String id) {
+        for (int index = 0; index < position; index++) {
+            if (items[index].getId().equals(id)) {
+                return items[index];
+            }
+        }
+        return null;
+
+    }
 }
---
+++ chapter_002/src/test/java/tracker/TrackerTest.java
@@ -0,0 +1,65 @@
+package tracker;
+import org.junit.Test;
+import ru.job4j.inheritance.Builder;
+import ru.job4j.tracker.Item;
+import ru.job4j.tracker.Tracker;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void trackerAdd() {
+        Item testItem = new Item("Сковорода");
+        Tracker testTracker = new Tracker();
+        assertThat(testTracker.add(testItem), is(testItem));
+    }
+
+    @Test
+    public void trackerFindAll() {
+        Tracker testTracker = new Tracker();
+        Item pan = testTracker.add(new Item("Сковорода"));
+        Item fork = testTracker.add(new Item("Вилка"));
+        Item mug = testTracker.add(new Item("Кружка"));
+        assertThat(testTracker.findAll(), is(new Item[]{pan, fork, mug}));
+    }
+
+    @Test
+    public void trackerFindByName() {
+        Tracker testTracker = new Tracker();
+        Item mug0 = testTracker.add(new Item("Кружка"));
+        Item pan = testTracker.add(new Item("Сковорода"));
+        Item fork = testTracker.add(new Item("Вилка"));
+        Item mug = testTracker.add(new Item("Кружка"));
+        assertThat(testTracker.findByName("Кружка"), is(new Item[] {mug0, mug}));
+    }
+
+    @Test
+    public void trackerFindByID() {
+        Tracker testTracker = new Tracker();
+        Item pan = testTracker.add(new Item("Сковорода"));
+        Item fork = testTracker.add(new Item("Вилка"));
+        Item mug = testTracker.add(new Item("Кружка"));
+        assertThat(testTracker.findById(mug.getId()), is(mug));
+    }
+
+    @Test
+    public void trackerFindByIDIfTheresNoOneThenNull() {
+        Tracker testTracker = new Tracker();
+        Item pan = testTracker.add(new Item("Сковорода"));
+        Item fork = testTracker.add(new Item("Вилка"));
+        Item mug = testTracker.add(new Item("Кружка"));
+        assertThat(testTracker.findById("153215462"), nullValue());
+    }
+}
