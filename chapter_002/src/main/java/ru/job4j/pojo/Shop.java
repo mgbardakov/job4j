69198d1b880b84ac713f09f7cfd0ab2c51a65d08
mgbardakov <mgbardakov@gmail.com>
2020-01-24 21:00:17 +0300

    3. Удаление моделей из массива.[#221038]

---
+++ chapter_002/src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,43 @@
+package ru.job4j.pojo;
+
+public class Shop {
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        products[3] = new Product("Pour", 19);
+        products[4] = new Product("Salt", 19);
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        products = new Shop().delete(products, 1);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+    }
+
+    public Product[] delete(Product[] products, int index) {
+        for (; index < products.length - 1; index++) {
+            products[index] = null;
+            products[index] = products[index + 1];
+        }
+        products[products.length - 1] = null;
+        return  products;
+    }
+}
