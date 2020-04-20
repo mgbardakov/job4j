ab9b542de9d84391af802fcca6c479292dd5e612
mgbardakov <mgbardakov@gmail.com>
2020-04-20 09:04:58 +0300

    1. Читаем файл конфигурации[#250549]

---
+++ junior/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,42 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.regex.Pattern;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        Pattern pattern = Pattern.compile("=");
+        try (BufferedReader read =  new BufferedReader(new FileReader(this.path))) {
+            read.lines().filter(x -> !(x.startsWith("##") || x.isEmpty()))
+                        .forEach(x -> values.put(pattern.split(x)[0], pattern.split(x)[1]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+}
---
+++ junior/src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("name"),
+                is("Maxim Bardakov")
+        );
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("name"),
+                is("Maxim Bardakov")
+        );
+    }
+
+    @Test
+    public void whenPairWithEmptyLine() {
+        String path = "./data/pair_with_empty_line.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("name"),
+                is("Maxim Bardakov")
+        );
+    }
+}
