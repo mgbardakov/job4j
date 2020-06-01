7ef6f7e2e7350a001283add70155c5166aedbc2a
mgbardakov <mgbardakov@gmail.com>
2020-06-01 09:07:51 +0300

    5.3.4. Перевернуть связанный список [#250516]
    (cherry picked from commit 2cc88c5a353e837adeb3bb3caac3188e6b77859d)

--- junior/src/main/java/ru/job4j/collection/ForwardLinked.java
+++ junior/src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -3,6 +3,12 @@ package ru.job4j.collection;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * forward linked element container
+ * @param <T> - element type
+ * @author mbardakov
+ * @since 28.05.2020
+ */
 public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
 
@@ -28,11 +34,51 @@ public class ForwardLinked<T> implements Iterable<T> {
         return rsl;
     }
 
+    /**
+     * reverts links in the list
+     */
+    public void revert() {
+        var firstNode = head;
+        if (firstNode == null) {
+            throw new UnsupportedOperationException("Empty list");
+        }
+        var secondNode = head.next;
+        if (secondNode == null) {
+            throw new UnsupportedOperationException("There's only one node in the list");
+        }
+        var thirdNode = secondNode.next;
+        firstNode.next = null;
+        secondNode.next = firstNode;
+
+        while (thirdNode != null) {
+            var nextNode = thirdNode.next;
+            thirdNode.next = secondNode;
+            thirdNode = nextNode;
+        }
+        head = secondNode;
+
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new Iterator<>() {
-            Node<T> node = head;
+            Iterator<Node<T>> nodeIterator = nodeIterator();
 
+            @Override
+            public boolean hasNext() {
+                return nodeIterator.hasNext();
+            }
+
+            @Override
+            public T next() {
+               return nodeIterator.next().value;
+            }
+        };
+    }
+
+    private Iterator<Node<T>> nodeIterator() {
+        return new Iterator<>() {
+            Node<T> node = head;
             @Override
             public boolean hasNext() {
                 return node != null;
@@ -43,9 +89,9 @@ public class ForwardLinked<T> implements Iterable<T> {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-                var rsl = node;
+                Node<T> value = node;
                 node = node.next;
-                return rsl;
+                return value;
             }
         };
     }
