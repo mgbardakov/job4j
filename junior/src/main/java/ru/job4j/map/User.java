c19ab10535615f32cabfa20df3b39e74217bb9c0
mgbardakov <mgbardakov@gmail.com>
2020-06-01 09:11:10 +0300

    1. Создать модель User [#250531]

---
+++ junior/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,14 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * user class
+ * @author mbardakov
+ * @since 01.06.2020
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+}
