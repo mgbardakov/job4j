38e8374755c99007c9594c9a2095f502999961ff
mgbardakov <mgbardakov@gmail.com>
2020-10-13 13:29:32 +0300

    3. Immutable объекты [#385425]

---
+++ middle/src/main/java/ru/job4j/concurrent/Node.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent;
+import net.jcip.annotations.Immutable;
+
+@Immutable
+public class Node<T> {
+    private final Node next;
+    private final T value;
+
+    public Node(Node next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
