5ef1839fe3ccde95bab79579e0f8db7526cd1614
mgbardakov <mgbardakov@gmail.com>
2021-03-06 18:32:36 +0300

    0.1. Генерация схемы через Hibernate [#385331]

--- chapter_002/src/main/java/ru/job4j/tracker/HibernateRun.java
+++ chapter_002/src/main/java/ru/job4j/tracker/HibernateRun.java
@@ -6,6 +6,8 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 public class HibernateRun {
@@ -26,6 +28,14 @@ public class HibernateRun {
             for (Item it : list) {
                 System.out.println(it);
             }
+            Item biba = Item.of("Biba",
+                    "Boba's brother",
+                    new Timestamp(new Date().getTime()));
+            Item boba = Item.of("Boba",
+                    "Brother of Biba",
+                    new Timestamp(new Date().getTime()));
+            create(biba, sf);
+            create(boba, sf);
         }  catch (Exception e) {
             e.printStackTrace();
         } finally {
--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 import java.util.Objects;
 @Entity
 @Table(name = "item")
@@ -9,17 +10,27 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+    private String description;
+    private Timestamp created;
 
     public Item() {
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public Item(String name) {
         this.name = name;
     }
 
-    public Item(int id, String name) {
-        this.id = id;
-        this.name = name;
+    public static Item of(String name, String description, Timestamp created) {
+        var item = new Item();
+        item.setName(name);
+        item.setDescription(description);
+        item.setCreated(created);
+        return item;
     }
 
     @Override
@@ -54,6 +65,22 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
     }
 
     @Override
